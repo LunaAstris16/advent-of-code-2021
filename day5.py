6bee917a3046c8847d823c463943144fe2468320
Luna Astris <lunaastris15@gmail.com>
2022-01-28 22:12:11 -0500

    new stuff for day5

--- day5.py
+++ day5.py
@@ -1,5 +1,15 @@
-testmain = str(open('testinputday4', 'r').read()).splitlines()
-cordinates = [ [0 for _ in range(1000)] for _ in range(1000)]
-print(cordinates)
+testmain = str(open('testinputday5.txt', 'r').read()).splitlines()
+cordinates = [ [0 for _ in range(10)] for _ in range(10)]
 for x in range(len(testmain)):
     currentVaule = testmain[x].split('->')
+    xvalueStart = int(currentVaule[0].split(',')[0])
+    yvauleStart = int(currentVaule[0].split(',')[1])
+    xvauleEnd = int(currentVaule[1].split(',')[0]) 
+    yvauleEnd = int(currentVaule[1].split(',')[1])
+    lengthDeltax = abs(xvauleEnd - xvalueStart)
+    lengthDeltay = abs(yvauleEnd - yvauleStart)
+    if lengthDeltax != 0:
+        for x in range(lengthDeltax):
+            print(x)
+        print('new range')
+print(cordinates)
